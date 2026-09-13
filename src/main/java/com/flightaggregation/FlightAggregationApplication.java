b00debb0f10eefcfb4e908cc506326979e0e0d50
Steampunk <dasies11@gmail.com>
2026-09-13 23:48:49 +0200

    chore: project setup

---
+++ src/main/java/com/flightaggregation/FlightAggregationApplication.java
@@ -0,0 +1,12 @@
+package com.flightaggregation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightAggregationApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(FlightAggregationApplication.class, args);
+    }
+}
